8fa6f4f45f5eac95e48dc66c85ecc03e370cc2c2
doaa elsayed <73131139+doaa10@users.noreply.github.com>
2023-05-25 00:07:52 +0300

    Add files via upload

---
+++ javascript/sign.js
@@ -0,0 +1,28 @@
+// const form=document.querySelector(".signup form");
+// const coninuebtn=form.querySelector(".button input");
+
+// console.log("hi");
+
+// form.onsubmit=(e)=>{
+//     e.preventDefault();
+// }
+
+// coninuebtn.onclick=()=>{
+    
+//     let xhr=new XMLHttpRequest();
+//     // xhr.open("POST","php/signup.php",true);
+//     xhr.onload=()=>{
+//         if(xhr.readyState===XMLHttpRequest.DONE){
+//            if(xhr.status===200){
+//             // let data=xhr.response;
+//             // console.log(data);
+//            }
+//         }
+
+//     }
+//     //send the form data theough ajax to phph
+//     let formData=new FormData(form);
+
+//     xhr.send(formData);
+
+// }
